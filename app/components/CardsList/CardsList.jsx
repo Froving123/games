89d51241e64df7dc476440faee48ed46e580c002
Froving123 <chernyh.sasha26@gmail.com>
2024-02-13 18:39:58 +0500

    7

--- app/components/CardsList/CardsList.jsx
+++ app/components/CardsList/CardsList.jsx
@@ -6,10 +6,10 @@ export const CardsList = (props) => {
   return (
     <section className={Styles["list-section"]}>
       <h2 className={Styles["list-section__title"]} id={props.id}>
-      {props.title}
+        {props.title}
       </h2>
       <ul className={Styles["cards-list"]}>
-      {props.children}
+        {props.id === "popular" ? <NewCardsFragment /> : <PopularCardsFragment />}
       </ul>
     </section>
   );
--- app/page.js
+++ app/page.js
@@ -1,15 +1,13 @@
 import { Banner } from "./components/Banner/Banner";
 import { CardsList } from "./components/CardsList/CardsList";
-import { NewCardsFragment } from "./components/CardsList/NewCardsFragment";
-import { PopularCardsFragment } from "./components/CardsList/PopularCardsFragment";
 import { Promo } from "./components/Promo/Promo";
 
 export default function Home() {
   return (
     <main>
       <Banner />
-      <CardsList />
-      <CardsList />
+      <CardsList id="popular" title="Популярные" />
+      <CardsList id="New" title="Новинки" />
       <Promo />
     </main>
   );
