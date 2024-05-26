66be2f8e9339371ac8b8271ebc53c2466992658e
Froving123 <chernyh.sasha26@gmail.com>
2024-05-26 18:15:40 +0500

    1

--- app/store/app-store.js
+++ app/store/app-store.js
@@ -20,7 +20,7 @@ export const useStore = create((set) => ({
             try {
                 const user = await getMe(endpoints.me, jwt);
                 if (user) {
-                    set({ isAuth: true, user, token: jwt });
+                    set({ isAuth: true, user: { ...user, id: user._id }, token: jwt });
                     setJWT(jwt);
                 } else {
                     set({ isAuth: false, user: null, token: null });
